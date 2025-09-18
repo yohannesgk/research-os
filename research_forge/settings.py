b42257f821498f44d2c805a0274c9b5fe74f3a40
Yohanne Gebrekirstos <125269076+yohannesgk@users.noreply.github.com>
2025-09-18 14:04:08 +0300

    Create admin user and fix authentication

--- research_forge/settings.py
+++ research_forge/settings.py
@@ -100,6 +100,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login URLs
-LOGIN_URL = '/auth/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
--- research_forge/urls.py
+++ research_forge/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('research.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
